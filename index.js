6f5564b5327d82d0cae563e109bd52fdf4f2dc53
jafow <jared.a.fowler@gmail.com>
2017-06-12 23:50:10 -0700

    more table settings

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 
-const addSong = document.getElementById('add-song')
+const addSong = document.getElementById('add-sound')
 const songTitle = document.getElementById('song-title')
 const songTag = document.getElementById('song-tag')
+const songFile = document.getElementById('song-file')
+const reader = new FileReader()
+var arch
+
+reader.addEventListener('loadend', (blob) => {
+  // arch.writeFile('~/Dat-Sounds/p2p-mix.mp3', blob, {encoding: 'binary'})
+  console.log('written');
+})
 
 addSong.addEventListener('click', (e) => {
-  var arch = DatArchive.create({
-    title: songTitle.value || 'Untitled',
-    description: songTag || ''
+  arch = DatArchive.create({
+    title: 'p2p-mix',
+    description: 'nice sounds'
   })
-  
+  console.log('p2ppp')
 })
+
+function handleFile(obj) {
+
+}
+

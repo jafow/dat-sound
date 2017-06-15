6c07e3c9061b2e2d2f96e6f86dd4b866fda58ce8
jafow <jared.a.fowler@gmail.com>
2017-06-15 14:03:13 -0700

    Moving things around; create the Dat right away
    OR on loadend after blob is read

--- index.js
+++ index.js
@@ -4,21 +4,31 @@ const songTitle = document.getElementById('song-title')
 const songTag = document.getElementById('song-tag')
 const songFile = document.getElementById('song-file')
 const reader = new FileReader()
-var arch
 
-reader.addEventListener('loadend', (blob) => {
-  arch.writeFile('p2p-mix.mp3', reader.result, {encoding: 'binary'})
-  console.log('after reading ', reader.result.toString())
+var arch = DatArchive.create({
+  title: 'p2p-mix',
+  description: 'nice sounds'
 })
 
-addSong.addEventListener('click', (e) => {
-  arch = DatArchive.create({
+reader.addEventListener('loadend', writeFileBlobXXX)
+
+// addSong.addEventListener('click', setupDat)
+
+async function setupDat(blob) {
+  console.log('settig up dat');
+  var arch = await DatArchive.create({
     title: 'p2p-mix',
     description: 'nice sounds'
   })
 
-  console.log('create Dat')
-})
+  await arch.writeFile('p2p-mix.mp3', reader.result, {encoding: 'binary'})
+  console.log('after reading ', reader.result.toString())
+}
+
+async function writeFileBlobXXX (blob) {
+  await arch.writeFile('p2p-mix.mp3', reader.result, {encoding: 'binary'})
+  console.log('after reading ', reader.result.toString())
+}
 
 function handleFile(files) {
   reader.readAsArrayBuffer(files[0])

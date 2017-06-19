f3e679f3cf81f7343520dd18bbf74be62b605898
jafow <jared.a.fowler@gmail.com>
2017-06-19 14:22:07 -0700

    Fix bug blocking archive from saving file blob correctly

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 
-const addSong = document.getElementById('add-sound')
-const songTitle = document.getElementById('song-title')
-const songTag = document.getElementById('song-tag')
-const songFile = document.getElementById('song-file')
+const addSound = document.getElementById('add-sound')
+const soundTitle = document.getElementById('sound-title')
+const soundTag = document.getElementById('sound-tag')
+const soundFile = document.getElementById('sound-file')
 const reader = new FileReader()
 
 
 reader.addEventListener('loadend', setupDat)
 
-// addSong.addEventListener('click', setupDat)
-
 async function setupDat(blob) {
-  console.log('settig up dat');
   var arch = await DatArchive.create({
-    title: 'p2p-mix',
-    description: 'nice sounds'
+    title: soundTitle.value || 'my dat sound',
+    description: soundTag.value || 'my nice dat sounds'
   })
 
-  await arch.writeFile('/test-blob.txt', new ArrayBuffer(16))
-  await arch.commit()
+  await arch.writeFile(soundTitle.value, reader.result)
+  arch.commit()
 }
 
 async function writeFileBlobXXX (blob) {
-  // await arch.writeFile('p2p-mix.mp3', reader.result, {encoding: 'binary'})
   console.log('after reading ', reader.result.toString())
 }
 
-async function handleFile(files) {
-  reader.readAsArrayBuffer(files[0])
+async function handleFile(fileList) {
+  reader.readAsArrayBuffer(fileList[0])
 }
 

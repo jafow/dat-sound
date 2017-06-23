d4203b9ba88316a848306f11edc074a6b20b7197
jafow <jared.a.fowler@gmail.com>
2017-06-23 14:10:45 -0700

    Make a playlist of audio files added

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const soundTitle = document.getElementById('sound-title')
 const soundTag = document.getElementById('sound-tag')
 const soundFile = document.getElementById('sound-file')
 const reader = new FileReader()
-
+var arch
 
 reader.addEventListener('loadend', setupDat)
+// document.addEventListener('DOMContentLoaded', makePlaylist)
 
 async function setupDat(blob) {
-  var arch = await DatArchive.create({
+  arch = await DatArchive.create({
     title: soundTitle.value || 'my dat sound',
     description: soundTag.value || 'my nice dat sounds'
   })
 
   await arch.writeFile(soundTitle.value, reader.result)
   arch.commit()
+  makePlaylist(arch)
 }
 
 async function writeFileBlobXXX (blob) {
@@ -26,3 +28,11 @@ async function handleFile(fileList) {
   reader.readAsArrayBuffer(fileList[0])
 }
 
+async function makePlaylist (archive) {
+  var audioFiles = await archive.readdir('/')
+  var playlist = document.getElementById('playlist')
+  for (let a of audioFiles) {
+    document.createElement('li')
+  }
+  return;
+}

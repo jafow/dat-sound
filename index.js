1c7f050afb9445899e721b4ab92852c49278f7b2
jafow <jared.a.fowler@gmail.com>
2017-07-03 08:25:53 -0700

    await uploadElement

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 /* global DatArchive */
 const yo = require('yo-yo')
 const uploadElement = require('upload-element')
-const soundTitle = document.getElementById('sound-title')
-const soundTag = document.getElementById('sound-tag')
 const createDat = document.getElementById('create-dat')
 const filePicker = document.getElementById('file-picker')
 const pickerTag = document.getElementById('file-picker-tag')
+const playlistSection = document.getElementById('playlist')
+const playlist = list([])
+
 var arch
 
 createDat.addEventListener('click', setupDat)
 pickerTag.addEventListener('click', clickFilePicker)
-
+document.addEventListener('DOMContentReady', popuplatePlaylist, {once: true})
 async function setupDat () {
   arch = await DatArchive.create({
     title: '',
@@ -22,23 +23,22 @@ async function addAudioFileToDat (err, file) {
   if (err) throw new Error(err)
   var fileName = cleanFileName(file[0].file.name)
   var buf = file[0].target.result
-
+  debugger;
   await arch.writeFile(fileName, buf)
   arch.commit()
   makePlaylist(arch)
 }
 
-function clickFilePicker (e) {
+async function clickFilePicker (e) {
   if (e.target === filePicker) {
     e.stopPropagation()
-    uploadElement(filePicker, addAudioFileToDat)
+    await uploadElement(filePicker, addAudioFileToDat)
   }
 }
 
 function cleanFileName (name) {
   // sanitize the file name according to
   // beaker web api
-  console.log('name: ', name);
   return 'test' + String(Date.now())
 }
 
